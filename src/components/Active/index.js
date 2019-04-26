308dcf5bcc4410aff2837f39a42826b66245a00e
Nicollas Carvalho <n.matheuscarvalho@gmail.com>
2019-04-25 23:29:05 -0300

    online offline

--- src/components/Active/index.js
+++ src/components/Active/index.js
@@ -1,28 +1,44 @@
 import React, { Component } from  'react'
-import { Platform, TouchableOpacity, Text } from 'react-native'
-import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete'
-import { View } from 'react-native'
-import MapView from 'react-native-maps'
+import { Platform, TouchableOpacity, Text, Alert } from 'react-native'
+import { connect } from 'react-redux'
+import { Spinner } from 'native-base'
+import * as firebase from 'firebase'
 
 class Active extends Component {
 	state = {
-		status: false
+		status: false,
+		loading: false
 	}
 
-	changeStatus = () => {
-		this.setState({
-			status: !this.state.status
+	changeStatus = async () => {
+		this.setState({ loading: true, status: !this.state.status},  async () => {
+			await firebase.database().ref(`register/commerce/motoboyPartner/${this.props.user.id}`).update({
+				rideStatus: this.state.status
+			})
+				.then(() => {
+					this.setState({ loading: false })
+				})
+				.catch(error => {
+					Alert.alert('Atenção', 'Houve um erro interno. Tente novamente em alguns instantes')
+					this.setState({ loading: false })
+					console.log('error updating ridestatus', error)
+			})
 		})
 	}
 
 	render(){
+		console.log('status do usuario', this.props.user)
 		return (
-				<TouchableOpacity style={[styles.container, { backgroundColor: this.state.status ? "green" : "red"}]} onPress={this.changeStatus}>
-					<Text style={styles.text}>{this.state.status ? "ONLINE" : "OFFLINE"}</Text>
-				</TouchableOpacity>
+			<TouchableOpacity style={[styles.container, { backgroundColor: this.state.status ? "green" : "red"}]} onPress={this.changeStatus}>
+				{this.state.loading ? <Spinner /> : <Text style={styles.text}>{this.state.status ? "ONLINE" : "OFFLINE"}</Text>}
+			</TouchableOpacity>
     )
 	}	
 }
+
+const mapStateToProps = state => ({
+	user: state.user.user
+})
 
 const styles = {
 	container: {
@@ -60,61 +76,4 @@ const styles = {
 	}
 }
 
-export default Active
-
-// styles={{
-// 	container: {
-// 		position: 'absolute',
-// 		top: Platform.select({
-// 			ios: 60, android: 40
-// 		}),
-// 		width: '100%'
-// 	},
-// 	textInputContainer: {
-// 		flex: 1,
-// 		backgroundColor: 'transparent',
-// 		heigth: 54,
-// 		marginHorizontal: 20,
-// 		borderTopWidth: 0,
-// 		borderBottomWidth: 0
-// 	},
-// 	textInput: {
-// 		height: 54,
-// 		margin: 0,
-// 		borderRadius: 0,
-// 		paddingTop: 0,
-// 		paddingBottom: 0,
-// 		paddingLeft: 20,
-// 		paddingRight: 20,
-// 		marginTop: 0,
-// 		marginLeft: 0,
-// 		marginRight: 0,
-// 		elevation: 5,
-// 		shadowColor: '#000',
-// 		shadowOpacity: 0.1,
-// 		shadowOffset: { x:0, y:0},
-// 		shadowRadius: 15,
-// 		borderWidth: 1,
-// 		borderColor: '#DDD',
-// 		fontSize: 18
-// 	},
-// 	listView: {
-// 		borderWidth: 1,
-// 		borderColor: '#DDD',
-// 		backgroundColor: '#FFF',
-// 		marginHorizontal: 20,
-// 		elevation: 5,
-// 		shadowColor: '#000',
-// 		shadowOpacity: 0.1,
-// 		shadowOffset: { x:0, y:0},
-// 		shadowRadius: 15,
-// 		marginTop: 10
-// 	},
-// 	description: {
-// 		fontSize: 16,
-// 	},
-// 	row: {
-// 		padding: 20,
-// 		height: 58
-// 	}
-// }}
+export default connect(mapStateToProps)(Active)
