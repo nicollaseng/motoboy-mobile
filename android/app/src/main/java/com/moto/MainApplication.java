561dddd1b1fe6abeaeace4859dc4faeb3f5c2d8a
Nicollas Carvalho <n.matheuscarvalho@gmail.com>
2019-05-15 00:19:40 -0300

    new packages and app working

--- android/app/src/main/java/com/moto/MainApplication.java
+++ android/app/src/main/java/com/moto/MainApplication.java
@@ -15,6 +15,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.staltz.reactnativeandroidlocalnotification.NotificationPackage;    // <- Add this line
 
 import java.util.Arrays;
 import java.util.List;
@@ -38,7 +39,8 @@ public class MainApplication extends Application implements ReactApplication {
             new VectorIconsPackage(),
             new RNGestureHandlerPackage(),
             new MapsPackage(),
-            new BackgroundTimerPackage()
+            new BackgroundTimerPackage(),
+            new NotificationPackage()          
       );
     }
 
